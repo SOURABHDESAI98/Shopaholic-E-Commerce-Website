712729df2ff63847c9bb9f58ba04d624a331f584
Sourabh <sourabhdesai008@gmail.com>
2023-06-02 13:01:04 +0530

    some changes done

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import { CartContextProvider } from './context/cart_context';
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const domain = process.env.REACT_APP_DOMAIN;
+const clientId = process.env.REACT_APP_CLIENT_ID;
 
 root.render(
 
   <Auth0Provider
-    domain='dev-tey5a0xdduy2pszl.us.auth0.com'
-    clientId='DRs1b5PLZ7X7Saccc412BXcfO5nzTpvU'
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
